49ead5aa59b01411b11c4bba230184497cc3b253
Jeison Mena <95304350+JeisonMena@users.noreply.github.com>
2025-06-28 09:53:02 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Astro v5.9.2">
+    <title>Signin Template · Bootstrap v5.3</title>
+    <script src="assets/js/color-modes.js"></script>
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="theme-color" content="#712cf9">
+    <link href="assets/css/sign-in.css" rel="stylesheet">
+</head>
+
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <main class="form-signin w-100 m-auto">
+        <form>
+            <div class="text-center">
+                <img class="mb-4" src="assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+            </div> 
+            <h1 class="h3 mb-3 fw-normal">Iniciar Sesion</h1>
+            <div class="form-floating">
+                <input name="correo" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Correo</label> </div>
+            <div class="form-floating">
+                <input name="clave" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Clave</label> </div>
+            <div class="my-3">
+                <button class="btn btn-primary w-100 py-2" type="submit">Ingresar</button>
+            </div>
+            <div class="text-center">
+                <p class="mt-5 mb-3 text-body-secondary">&copy; 2025</p>
+            </div>
+        </form>
+    </main>
+    <script src="assets/dist/js/bootstrap.bundle.min.js" class="astro-vvvwv3sm"></script>
+</body>
+
+</html>
